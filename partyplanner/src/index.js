d76b052aff500419e1b0d7afcfa6c9b4a107d594
unknown <will.ediger@gmail.com>
2019-07-10 19:38:37 -0500

    added redux, set up redux store

--- partyplanner/src/index.js
+++ partyplanner/src/index.js
@@ -3,4 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { createStore, applyMiddleware } from "redux"; // importing applyMiddleware to be able to add thunk later on
+import { Provider } from "react-redux";
+
+import reducer from "./reducers"; // needs to be built
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
