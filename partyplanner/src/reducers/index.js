8605052ada8674188d96eb78cec018ae76dffe20
unknown <will.ediger@gmail.com>
2019-07-10 19:51:49 -0500

    created initial state for reducer

---
+++ partyplanner/src/reducers/index.js
@@ -0,0 +1,10 @@
+const initialState = {
+  parties: [],
+  loginToken: null,
+  loggingIn: false,
+  fetchingParties: false,
+  addingParty: false,
+  updatingParty: false,
+  deletingParty: false,
+  error: null
+};
