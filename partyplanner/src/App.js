91c4bf982f49980334411e620388ddab626d6b6f
unknown <will.ediger@gmail.com>
2019-07-10 19:57:19 -0500

    removed CRA default app page contents

--- partyplanner/src/App.js
+++ partyplanner/src/App.js
@@ -1,24 +1,9 @@
 import React from "react";
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Party Planner will eventually live here.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
