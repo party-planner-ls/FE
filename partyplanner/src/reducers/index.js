2a3d535e65736ee231e0f09b948c6b33a74f339c
unknown <will.ediger@gmail.com>
2019-07-10 19:53:13 -0500

    created reducer, added cases for handling login

--- partyplanner/src/reducers/index.js
+++ partyplanner/src/reducers/index.js
@@ -1,3 +1,5 @@
+import { LOGIN_START, LOGIN_SUCCESS, LOGIN_FAILURE } from "../Actions";
+
 const initialState = {
   parties: [],
   loginToken: null,
@@ -8,3 +10,32 @@ const initialState = {
   deletingParty: false,
   error: null
 };
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOGIN_START:
+      return {
+        ...state,
+        error: null,
+        loggingIn: true
+      };
+
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        error: null,
+        loginToken: action.payload
+      };
+
+    case LOGIN_FAILURE:
+      return {
+        ...state,
+        loggingIn: false,
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
