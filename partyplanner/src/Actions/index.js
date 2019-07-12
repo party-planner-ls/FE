36fae02fd91906dfe69865acaba7c8305a0478d1
unknown <will.ediger@gmail.com>
2019-07-11 21:28:15 -0500

    added "getParties" action creator

--- partyplanner/src/Actions/index.js
+++ partyplanner/src/Actions/index.js
@@ -1,43 +1,45 @@
 import axios from "axios";
 
-export const REGISTER_START = 'REGISTER_START';
-export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
-export const REGISTER_FAILURE = 'REGISTER_FAILURE';
+export const REGISTER_START = "REGISTER_START";
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAILURE = "REGISTER_FAILURE";
 export const LOGIN_START = "LOGIN_START";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAILURE = "LOGIN_FAILURE";
 export const GET_TODOS = "GET_TODOS";
 export const GET_TODOS_SUCCESS = "GET_TODOS_SUCCESS";
 export const GET_TODOS_FAILED = "GET_TODOS_FAILED";
-export const PARTY_START = 'PARTY_START';
-export const PARTY_SUCCESS = 'PARTY_SUCCESS';
-export const PARTY_FAILURE = 'PARTY_FAILURE';
-
+export const PARTY_START = "PARTY_START";
+export const PARTY_SUCCESS = "PARTY_SUCCESS";
+export const PARTY_FAILURE = "PARTY_FAILURE";
+export const FETCH_PARTIES_START = "FETCH_PARTIES_START";
+export const FETCH_PARTIES_SUCCESS = "FETCH_PARTIES_SUCCESS";
+export const FETCH_PARTIES_FAILURE = "FETCH_PARTIES_FAILURE";
 
 //placeholder for url
 const URL = "placeholder";
 
 export const Register = credentials => dispatch => {
   dispatch({
-      type: REGISTER_START
+    type: REGISTER_START
   });
-  return axios 
-  .post(URL, credentials)
-  .then(res => {
+  return axios
+    .post(URL, credentials)
+    .then(res => {
       console.log(res);
       dispatch({
-          type: REGISTER_SUCCESS,
-          payload: res.data
+        type: REGISTER_SUCCESS,
+        payload: res.data
       });
-  })     
-  .catch(err => {
+    })
+    .catch(err => {
       console.log(err);
       dispatch({
-          type: REGISTER_FAILURE,
-          payload: err
-      })
-  })
-}
+        type: REGISTER_FAILURE,
+        payload: err
+      });
+    });
+};
 
 export const LOGIN = credentials => dispatch => {
   dispatch({
@@ -71,15 +73,28 @@ export const getTodos = () => dispatch => {
 };
 
 export const party = () => dispatch => {
-  dispatch({type: PARTY_START});
-  let userName = 'placeHolder';
+  dispatch({ type: PARTY_START });
+  let userName = "placeHolder";
   return axios
     .get(URL)
     .then(res => {
       console.log(res.data);
-      dispatch({type: PARTY_SUCCESS, payload: res.data})
+      dispatch({ type: PARTY_SUCCESS, payload: res.data });
+    })
+    .catch(err => {});
+};
+
+export const getParties = () => dispatch => {
+  dispatch({ type: FETCH_PARTIES_START });
+  axios
+    .get(URL)
+    .then(res => {
+      dispatch({
+        type: FETCH_PARTIES_SUCCESS,
+        payload: res.data
+      });
     })
     .catch(err => {
-
+      dispatch({ type: FETCH_PARTIES_FAILURE, payload: err.response });
     });
-}
+};
