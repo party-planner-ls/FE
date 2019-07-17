5f1b080b30a0cb3b5a52fc0f1beded962e12df21
unknown <will.ediger@gmail.com>
2019-07-16 21:34:50 -0500

    added getShoppingList action creator

--- partyplanner/src/Actions/index.js
+++ partyplanner/src/Actions/index.js
@@ -18,6 +18,9 @@ export const FETCH_PARTIES_FAILURE = "FETCH_PARTIES_FAILURE";
 export const DELETE_PARTY_START = "DELETE_PARTY_START";
 export const DELETE_PARTY_SUCCESS = "DELETE_PARTY_SUCCESS";
 export const DELETE_PARTY_FAILURE = "DELETE_PARTY_FAILURE";
+export const GET_SHOPPING_LIST_START = "GET_SHOPPING_LIST_START";
+export const GET_SHOPPING_LIST_SUCCESS = "GET_SHOPPING_LIST_SUCCESS";
+export const GET_SHOPPING_LIST_FAILURE = "GET_SHOPPING_LIST_FAILURE";
 
 //placeholder for url
 const URL = "placeholder";
@@ -114,5 +117,20 @@ export const deleteParty = id => dispatch => {
     })
     .catch(err => {
       dispatch({ type: DELETE_PARTY_FAILURE, payload: err.response });
+    });
+};
+
+export const getShoppingList = partyId => dispatch => {
+  dispatch({ type: GET_SHOPPING_LIST_START });
+  axios
+    .get(`URL/party/${partyId}/list`)
+    .then(res => {
+      dispatch({
+        type: GET_SHOPPING_LIST_SUCCESS,
+        payload: res.data
+      });
+    })
+    .catch(err => {
+      dispatch({ type: GET_SHOPPING_LIST_FAILURE, payload: err.response });
     });
 };
