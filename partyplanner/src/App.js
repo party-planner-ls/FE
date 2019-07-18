e00349d4fe4d83f4547d4c9d7b2d752861610505
unknown <will.ediger@gmail.com>
2019-07-18 18:57:09 -0500

    converted App to class Component, imported NavLink

--- partyplanner/src/App.js
+++ partyplanner/src/App.js
@@ -1,47 +1,65 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import "./App.css";
 
-import Login from './components/Login';
-import Register from './components/Register';
+import Login from "./components/Login";
+import Register from "./components/Register";
 import Parties from "./components/Parties";
 import Party from "./components/Party";
 
-
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
   logout = e => {
     e.preventDefault();
-    localStorage.removeItem('userId');
-    localStorage.removeItem('token');
-  }
+    localStorage.removeItem("userId");
+    localStorage.removeItem("token");
+  };
+
+  render() {
     return (
       <Router>
         <div className="App">
           <nav>
             <ul>
-              {localStorage.getItem('token') ? (
+              {localStorage.getItem("token") ? (
                 <>
-                <NavLink to = '/' exact activeClassName = 'current'>Home</NavLink>
-                <NavLink to = '/login'n exact activeClassName = 'current'>Login</NavLink>
-                <NavLink to = '/register' exact activeClassName = 'current'>Register</NavLink>
+                  <NavLink to="/" exact activeClassName="current">
+                    Home
+                  </NavLink>
+                  <NavLink to="/login" n exact activeClassName="current">
+                    Login
+                  </NavLink>
+                  <NavLink to="/register" exact activeClassName="current">
+                    Register
+                  </NavLink>
                 </>
-                ):(
+              ) : (
                 <React.Fragment>
-                <NavLink to = '/parties' exact activeClassName = 'current'>Parties</NavLink>
-                <NavLink to = '//parties/:id' exact activeClassName = 'current'>Party</NavLink>
-                <button className = 'submitBtn' onClick = {this.logout} to = '/' >Logout</button>
+                  <NavLink to="/parties" exact activeClassName="current">
+                    Parties
+                  </NavLink>
+                  <NavLink to="//parties/:id" exact activeClassName="current">
+                    Party
+                  </NavLink>
+                  <button className="submitBtn" onClick={this.logout} to="/">
+                    Logout
+                  </button>
                 </React.Fragment>
-                )}
+              )}
             </ul>
           </nav>
-          <Route exact path ='/login' component={Login}/>
-          <Route exact path='/register' component={Register}/>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
           <Route exact path="/parties" component={Parties} />
           <Route exact path="/parties/:id" component={Party} />
         </div>
       </Router>
     );
+  }
 }
 
 export default App;
