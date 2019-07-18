d60c4363b0d636e9848de39f1c98702df432c979
unknown <will.ediger@gmail.com>
2019-07-18 18:35:04 -0500

    added ability to delete shopping list item

---
+++ partyplanner/src/components/DeleteModal.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+export default function DeleteModal(props) {
+  // this is pulled from the Material-UI Form Dialog example
+  // it uses react hooks, which allows you to utilize state on functional components
+  const { onClose, ...other } = props;
+
+  function handleClose() {
+    onClose(false);
+  }
+
+  function handleSubmit() {
+    onClose(true);
+  }
+
+  return (
+    <Dialog
+      onClose={handleClose}
+      aria-labelledby="form-dialog-title"
+      {...other}
+    >
+      <DialogTitle id="form-dialog-title">Confirm Deletion</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Delete {props.item.name}?
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="primary">
+          Cancel
+        </Button>
+        <Button onClick={handleSubmit} color="primary">
+          Delete
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
