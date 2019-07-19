404c372cae66a1d9c870b0b0fb5ab03b7c0951c2
unknown <will.ediger@gmail.com>
2019-07-18 19:00:17 -0500

    fixed import of React & React.Component

--- partyplanner/src/components/Register.js
+++ partyplanner/src/components/Register.js
@@ -1,4 +1,4 @@
-import { React, Component } from "react";
+import React, { Component } from "react";
 import { Register } from "../Actions";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
