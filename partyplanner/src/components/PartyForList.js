766207ae2d4e06518866f6d947e0b4e5cde247e3
unknown <will.ediger@gmail.com>
2019-07-18 19:15:07 -0500

    removed unnecessary imports

--- partyplanner/src/components/PartyForList.js
+++ partyplanner/src/components/PartyForList.js
@@ -1,8 +1,5 @@
 import React from "react";
 
-import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
-
 import { deleteParty } from "../Actions";
 
 const PartyForList = props => {
--- partyplanner/src/components/ShoppingListItem.js
+++ partyplanner/src/components/ShoppingListItem.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 import PurchaseModal from "./PurchaseModal";
 import DeleteModal from "./DeleteModal";
