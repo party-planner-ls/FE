e4fd9c23bf5708a2e632fb7fbfc922e96e49b92a
shian simms <ssimmswork@gmail.com>
2019-07-20 19:18:55 -0400

    uncommit

--- partyplanner/src/components/Icon.js
+++ partyplanner/src/components/Icon.js
@@ -1,4 +1,4 @@
-/*import React from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/styles";
 import { Icon as IconOriginal } from "@material-ui/core";
 
@@ -15,4 +15,4 @@ export default function Icon(props) {
       {props.name}
     </IconOriginal>
   );
-}*/
+}
--- partyplanner/src/components/PurchaseModal.js
+++ partyplanner/src/components/PurchaseModal.js
@@ -1,4 +1,4 @@
-/*import React, { useState } from "react";
+import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
@@ -64,4 +64,3 @@ export default function PurchaseModal(props) {
     </Dialog>
   );
 }
-*/
