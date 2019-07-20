f27b80aa4b884e2aeca6185e111245fb9676555a
shian simms <ssimmswork@gmail.com>
2019-07-19 23:40:20 -0400

    loader

--- partyplanner/src/components/Register.js
+++ partyplanner/src/components/Register.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Register } from "../Actions";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
+import Loader from 'react-loader-spinner';
 import './App.css';
 
 class Registration extends Component {
