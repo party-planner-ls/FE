11ee5df6a5e6022f312b1b845cadff19d0cc06fd
unknown <will.ediger@gmail.com>
2019-07-20 08:27:38 -0500

    fixed devsettings syntax error

--- partyplanner/src/config/index.js
+++ partyplanner/src/config/index.js
@@ -2,6 +2,6 @@ export const devMode = false;
 
 export const devSettings = {
   devToken:
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0IjoyLCJlbWFpbCI6IndpbGxGcmlkYXlAZXhhbXBsZS5jb20iLCJpYXQiOjE1NjM2Mjc4NjAsImV4cCI6MTU2MzcxNDI2MH0.s7pctjbQ-CyCmz_Ch-7DQtSJJQccJKOQ-EqZZCIi1gg",
-  devUserId = 2
-}
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0IjoyLCJlbWFpbCI6IndpbGxGcmlkYXlAZXhhbXBsZS5jb20iLCJpYXQiOjE1NjM2Mjc4NjAsImV4cCI6MTU2MzcxNDI2MH0.s7pctjbQ-CyCmz_Ch-7DQtSJJQccJKOQ-EqZZCIi1gg",
+  devUserId: 2
+};
