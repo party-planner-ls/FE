d1735a675678386532d876bf9347cbe2efd7a4af
shian simms <ssimmswork@gmail.com>
2019-07-22 22:15:35 -0400

    resolving confilcts

--- partyplanner/src/components/HomePage.js
+++ partyplanner/src/components/HomePage.js
@@ -1,9 +1,6 @@
 import React,{ Component} from 'react';
 import './App.css';
 import { NavLink } from 'react-router-dom';
-import React, { Component } from "react";
-import "./App.css";
-import { NavLink } from "react-router-dom";
 
 class HomePage extends Component {
     render(){
@@ -11,29 +8,18 @@ class HomePage extends Component {
             <div className = 'pageLayout'>
                 <div className = 'content'>
                     <h2>Welcome To The Best <br/> Party Planner <br/> App On The Web</h2>
+                    <div className = 'home'>
                     <p>Wanna see what the hype is about?</p>
                     <NavLink to = '/register'>Sign up Now!</NavLink>
+                    </div>
+                    <div className = 'home'>
                     <p>Already apart of the family?</p>
-                    <NavLink to = '/login'>Sign up Now!</NavLink>
+                    <NavLink to = '/login'>Login Now!</NavLink>
+                    </div>
             </div>
             </div>
         );
     }
-  render() {
-    return (
-      <div className="pageLayout">
-        <div className="content">
-          <h2>
-            Welcome To The Best <br /> Party Planner <br /> App On The Web
-          </h2>
-          <p>Wanna see what the hype is about?</p>
-          <NavLink to="/register">Sign up Now!</NavLink>
-          <p>Already apart of the family?</p>
-          <NavLink to="/login">Log in Now!</NavLink>
-        </div>
-      </div>
-    );
-  }
 }
 
 export default HomePage;
